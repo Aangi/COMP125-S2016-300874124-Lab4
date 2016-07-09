b32a7531b10a85a34d1c424b7cd94fa146350ffa
Aangi <aashah.6198@gmail.com>
2016-07-08 22:11:50 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,45 @@
+
+//Filename : app.js
+//@author Aayushi Shah
+//@date July 08,2016
+//StudentID : 300874124
+//website : 
+//@description : This file is the main javascript of this file.
+//*/
+// IIFE = Immediately Invoked Function Expression
+(function () {
+
+    // Code goes here
+    "use strict";
+
+        // Event listner with inline anonymous event handler function
+        LoginPage.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        formInputOnConsole();
+
+        LoginPage.reset();
+        })
+
+
+
+
+    // formInputOnConsole function 
+    function formInputOnConsole() {
+
+        //creating user object
+        function User(username, password)
+        {
+            this.userName = username;
+            this.Password = password;
+        }
+
+        var user = new User(username.value, password.value)
+
+        console.log(user);
+ 
+    }
+
+
+
+})()
